1eff136d769178edd53f05aaa87894d164ba31b9
Juan Antonio Carrasco del Cid <77850813E_aea>
2026-06-28 10:26:59 +0200

    Project config

---
+++ ParkingKeeperTests/ParkingKeeperTests.swift
@@ -0,0 +1,16 @@
+//
+//  ParkingKeeperTests.swift
+//  ParkingKeeperTests
+//
+//  Created by Juan Antonio Carrasco del Cid on 28/6/26.
+//
+
+import Testing
+
+struct ParkingKeeperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
